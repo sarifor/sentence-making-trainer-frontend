2584ac15329336c5630c8998b0684ed87e9accf9
sarifor <iread1q84@gmail.com>
2022-07-14 17:11:09 +0900

    Add Upload component

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,7 +1,30 @@
 function Upload() {
   return (
     <div className="Upload">
-      Upload!
+      <div className="table__top">
+        <h1 className="table__top__title">
+          Upload
+        </h1>
+        <a className="btn" href="/records">
+          List
+        </a>
+      </div>
+
+      <div className="form">
+        <form method="POST" enctype="application/x-www-form-urlencoded">
+          <div className="form__input">
+            <label>Sentence</label>
+            <input name="sentence" type="text" />
+            <label>Source:</label>
+            <input name="source" type="text" />
+          </div>
+
+          <div className="form__buttons">
+            <input className="btn" type="submit" value="Upload" />
+            <input className="btn" type="submit" value="Cancel" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
