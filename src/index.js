8c792e8de0e59b644fe73afe7d7613723958295e
sarifor <iread1q84@gmail.com>
2022-07-17 12:36:01 +0900

    Get records from server

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './components/app';
 import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/grqphql',
+  uri: 'http://localhost:4000/graphql/',
   cache: new InMemoryCache(),
 })
 
