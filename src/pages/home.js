e3ba42afc5ae0516ab433bb7453ecfea226f5e78
sarifor <iread1q84@gmail.com>
2022-08-02 12:22:13 +0900

    Improve getting records from server

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,6 @@
 import { useQuery, gql } from '@apollo/client';
 
 function Home() {
-  const records = {
-    index: 1,
-    sentence: "aaa",
-    translated: "bbb",
-    source: "ccc",
-    uploaded: "ddd",
-  };
-
   const GET_RECORDS = gql`
     {
       showAllRecords {
@@ -23,41 +15,50 @@ function Home() {
     }
   `
 
-  const result = useQuery(GET_RECORDS);
-  console.log(result);
+  const { loading, error, data } = useQuery(GET_RECORDS);
 
-  return (
-    <div className="Home">
-      <div className="table__top">
-        <h1 className="table__top__title">
-          List
-        </h1>
-        <a className="btn" href="/records/upload">
-          Upload
-        </a>
+  if (loading) {
+    return <p>Loading ...</p>;
+  } else {
+    if (error) return <p>${error.message}</p>
+
+    const records = data.showAllRecords.records;
+
+    return (
+      <div className="Home">
+        <div className="table__top">
+          <h1 className="table__top__title">
+            List
+          </h1>
+          <a className="btn" href="/records/upload">
+            Upload
+          </a>
+        </div>
+  
+        <div className="table">
+          <div><h4>No.</h4></div>
+          <div><h4>Sentence</h4></div>
+          <div><h4>Translated</h4></div>
+          <div><h4>Source</h4></div>
+          <div><h4>Uploaded</h4></div>
+  
+          {records ? (
+            records.map((record) => 
+              <>
+                <div><h4><a href={`/records/${records.index}/edit`}>{record.index}</a></h4></div>
+                <div><h4>{record.sentence}</h4></div>
+                <div><h4>{record.translated}</h4></div>
+                <div><h4>{record.source}</h4></div>
+                <div><h4>{record.uploaded}</h4></div>          
+              </>
+            )
+          ) : (
+            <div><h4>No Value yet</h4></div>
+          )}
+        </div>
       </div>
-
-      <div className="table">
-        <div><h4>No.</h4></div>
-        <div><h4>Sentence</h4></div>
-        <div><h4>Translated</h4></div>
-        <div><h4>Source</h4></div>
-        <div><h4>Uploaded</h4></div>
-
-        {records ? (
-          <>
-            <div><h4><a href={`/records/${records.index}/edit`}>{records.index}</a></h4></div>
-            <div><h4>{records.sentence}</h4></div>
-            <div><h4>{records.translated}</h4></div>
-            <div><h4>{records.source}</h4></div>
-            <div><h4>{records.uploaded}</h4></div>          
-          </>
-        ) : (
-          <div><h4>No Value yet</h4></div>
-        )}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
