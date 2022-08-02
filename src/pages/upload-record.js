c810267a020cba44502907f13478828381581e57
sarifor <iread1q84@gmail.com>
2022-08-02 13:02:22 +0900

    Improve uploading record to server

--- src/pages/upload-record.js
+++ src/pages/upload-record.js
@@ -2,9 +2,6 @@ import { useNavigate } from "react-router-dom";
 import { useMutation, gql } from '@apollo/client';
 
 function Upload() {
-  const tempSentence = "eee";
-  const tempSource = "fff";
-
   let navigate = useNavigate();
 
   const UPLOAD_RECORD = gql`
@@ -16,6 +13,21 @@ function Upload() {
   `
 
   const [uploadRecord] = useMutation(UPLOAD_RECORD);
+
+  const handleClick = () => {
+    const sentence = document.getElementById("sentence");
+    const source = document.getElementById("source");    
+
+    uploadRecord({ 
+      variables: { 
+        record: { 
+          sentence: sentence.value,
+          source: source.value,
+        }
+      }
+    });
+    navigate("/records");
+  };
 
   return (
     <div className="Upload">
@@ -31,32 +43,20 @@ function Upload() {
       <div className="form">
         <form method="POST" enctype="application/x-www-form-urlencoded" onSubmit={e => {
           e.preventDefault();
-          uploadRecord({ 
-            variables: { 
-              record: { 
-                sentence: "Finally done", 
-                source: "me", 
-              }
-            }
-          });
         }}>
           <div className="form__input">
             <label>Sentence</label>
-            <input name="sentence" type="text" />
+            <input name="sentence" id="sentence" type="text" />
             <label>Source:</label>
-            <input name="source" type="text" />
+            <input name="source" id="source" type="text" />
           </div>
 
           <div className="form__buttons">
-            <input className="btn" value="Upload" onClick={() => {
-              uploadRecord(tempSentence, tempSource);
-              navigate("/records");
-            }}/>
+            <input className="btn" value="Upload" onClick={() => handleClick()} />
             <input className="btn" value="Cancel" onClick={() => {
               navigate("/records");
             }} />
           </div>
-          <button type="submit">Upload to GraphQL API Server</button>
         </form>
       </div>
     </div>
